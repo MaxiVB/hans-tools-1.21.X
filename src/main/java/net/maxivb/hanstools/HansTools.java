782b0b03c2c6bdf56a3a183d07026a2b99341c4a
MaxiVB <Redbullgamer@gmx.de>
2024-10-22 18:05:12 +0200

    Initial commit

---
+++ src/main/java/net/maxivb/hanstools/HansTools.java
@@ -0,0 +1,16 @@
+package net.maxivb.hanstools;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class HansTools implements ModInitializer {
+	public static final String MOD_ID = "hanstools";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/maxivb/hanstools/HansToolsClient.java
@@ -0,0 +1,10 @@
+package net.maxivb.hanstools;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class HansToolsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
