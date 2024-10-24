46f777a95caaa2a659f64f3617fe7dcfcf2c6dcc
MaxiVB <Redbullgamer@gmx.de>
2024-10-24 18:21:29 +0200

    added important comment

--- src/main/java/net/maxivb/hanstools/block/ModBlocks.java
+++ src/main/java/net/maxivb/hanstools/block/ModBlocks.java
@@ -1,19 +1,24 @@
 package net.maxivb.hanstools.block;
 
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.maxivb.hanstools.HansTools;
 import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.item.ToolItem;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.Identifier;
 
+import javax.tools.Tool;
+
 public class ModBlocks {
     public static final Block RUBY_BLOCK = registerBlock("ruby_block",
-            new Block(AbstractBlock.Settings.create().strength(4f)
-                    .requiresTool().sounds(BlockSoundGroup.AMETHYST_BLOCK).))
+            new Block(AbstractBlock.Settings.create().strength(1f)
+                    .requiresTool().sounds(BlockSoundGroup.COPPER)));
 
     private static Block registerBlock(String name, Block block) {
         registerBlockItem(name, block);
@@ -27,5 +32,9 @@ public class ModBlocks {
 
     public static void registerModBlocks() {
         HansTools.LOGGER.info("Registering Mod Blocks for " + HansTools.MOD_ID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.BUILDING_BLOCKS).register(fabricItemGroupEntries -> {
+            fabricItemGroupEntries.add(ModBlocks.RUBY_BLOCK);
+        });
     }
 }
